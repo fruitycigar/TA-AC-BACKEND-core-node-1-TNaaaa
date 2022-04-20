c0798be59352dcb648565a86447ba470876fad7e
fruitycigar <shruthisagar@SHODAN.local>
2022-04-20 12:57:28 +0530

    Node Assignment I

---
+++ block-BNaaaj/server.js
@@ -0,0 +1,8 @@
+var { parse } = require('url');
+
+var parsedUrl = parse(`https://airindia.com/fares/calculate?from=delhi&to=detroit`);
+
+console.log(parsedUrl.query);
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.protocol);
+console.log(parse(parsedUrl.query, true));
